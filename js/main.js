5d532040df28a8fe9947c245e752453cf0325efb
dapishta <dapishta@gmail.com>
2019-06-12 18:19:18 +0800

    Fixed problems

--- js/main.js
+++ js/main.js
@@ -2,13 +2,7 @@
 'use strict';
 
 // Tags
-var PHOTO_LIST_TAG = document.querySelector('.pictures');
-var PHOTO_ITEM_TAG = document
-  .querySelector('#picture')
-  .content.querySelector('.picture');
-var PHOTO_ITEM_IMG_TAG = PHOTO_ITEM_TAG.querySelector('.picture__img');
-var PHOTO_ITEM_LIKES_TAG = PHOTO_ITEM_TAG.querySelector('.picture__likes');
-var PHOTO_ITEM_COMMENTS_TAG = PHOTO_ITEM_TAG.querySelector('.picture__comments');
+var NUMBER_OF_PHOTOS = 25;
 
 // Data
 var comments = [
@@ -33,74 +27,81 @@ var names = [
   'Стас'
 ];
 
-// Functions
 
-// Create random number from to. User can enter one or two number in ascending or descending order.
+// Create random number from min max range.
 
 var getRandomNumber = function (min, max) {
-  if (max) {
-    if (min > max) {
-      return Math.round(Math.random() * (min - max) + max);
-    }
-    return Math.round(Math.random() * (max - min) + min)
-  }
-  return Math.round(Math.random() * min)
+  return Math.round(Math.random() * (max - min) + min)
+};
+
+var getString = function (array, min, max) {
+  var randomNumber = Math.round(Math.random() * (max - min) + min);
+  return array[randomNumber];
 };
 
 
-// Create data object
+// Create mock data
 
-var createDataObject = function (numberOfItems, nameOfTheArrayToBeFilled) {
+var createMockData = function (numberOfItems) {
+  var array = [];
+
   for (var i = 0; i < numberOfItems; i++) {
     // Comments. Get number of comments we want for a post. Create array for this post comments and fill this array with random comments.
     var numberOfComments = getRandomNumber(0, comments.length - 1);
-    var randomComments = [];
+    var photoComments = [];
     for (var y = 0; y < numberOfComments; y++) {
-      var randomComment = getRandomNumber(i, comments.length - 1);
-      randomComments[y] = comments[randomComment];
+      photoComments[y] = getString(comments, 0, comments.length - 1);
     }
 
     // Likes. Get number.
     var numberOfLikes = getRandomNumber(200, 15);
 
     // Get number for name
-    var randomName = getRandomNumber(names.length - 1, i);
+    var randomName = getString(names, 0, names.length - 1);
 
     // Fill the data object with details
-    nameOfTheArrayToBeFilled[i] = {
+    array[i] = {
       url: 'photos/' + (i + 1) + '.jpg',
       likes: numberOfLikes,
-      comment: randomComments,
-      name: names[randomName]
+      comment: photoComments,
+      name: randomName
     };
   }
+  return array;
 };
 
 // Create DOM Elements
-var createDOMElements = function (array, fragment) {
+var createDOMElements = function (array) {
+  var photoItemTag = document
+    .querySelector('#picture')
+    .content.querySelector('.picture');
+  var photoItemImgTag = photoItemTag.querySelector('.picture__img');
+  var photoItemLikesTag = photoItemTag.querySelector('.picture__likes');
+  var photoItemCommentsTag = photoItemTag.querySelector('.picture__comments');
+  var fragment = document.createDocumentFragment();
   for (var i = 0; i < array.length; i++) {
-    PHOTO_ITEM_IMG_TAG.src = array[i].url;
-    PHOTO_ITEM_LIKES_TAG.textContent = array[i].likes;
+    photoItemImgTag.src = array[i].url;
+    photoItemLikesTag.textContent = array[i].likes;
     for (var u = 0; u < array[i].comment.length; u++) {
-      PHOTO_ITEM_COMMENTS_TAG.textContent += array[i].comment[u];
+      photoItemCommentsTag.textContent += array[i].comment[u];
     }
-    fragment.appendChild(PHOTO_ITEM_TAG.cloneNode(true));
+    fragment.appendChild(photoItemTag.cloneNode(true));
   }
+  return fragment;
 };
 
-// Show elements in browser
-var renderFragment = function (tagToInsert, fragment) {
-  tagToInsert.appendChild(fragment);
+// Insert tag inside other tag
+var insertTag = function (tagToBeInserted, tagWhereToInsert) {
+  tagWhereToInsert.appendChild(tagToBeInserted);
 };
 
 // Execute all
 
 var renderPhotos = function (numberOfPhotos) {
-  var photosData = [];
-  var fragment = document.createDocumentFragment();
-  createDataObject(numberOfPhotos, photosData);
-  createDOMElements(photosData, fragment);
-  renderFragment(PHOTO_LIST_TAG, fragment);
+  var photoListTag = document.querySelector('.pictures');
+  var photos = createMockData(numberOfPhotos);
+  var createdPhotosFragmentTag = createDOMElements(photos);
+  insertTag(createdPhotosFragmentTag, photoListTag);
 };
 
-renderPhotos(25);
+renderPhotos(NUMBER_OF_PHOTOS);
